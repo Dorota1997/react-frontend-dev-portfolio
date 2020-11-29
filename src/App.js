2d7bdc66e3285aa78605e0e8d0b0ab214d6db575
Paweł Idzikowski <iddziku@gmail.com>
2020-11-29 21:17:16 +0100

    Use global variables to manipulate picked language

--- src/App.js
+++ src/App.js
@@ -1,39 +1,56 @@
-import React, { Component } from 'react';
-import $ from 'jquery';
-import './App.scss';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import About from './components/About';
-import Experience from './components/Experience';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
+import React, { Component } from "react";
+import $ from "jquery";
+import "./App.scss";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import Experience from "./components/Experience";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
 
 class App extends Component {
   constructor(props) {
     super();
     this.state = {
-      foo: 'bar',
-      resumeData: {}
+      foo: "bar",
+      resumeData: {},
     };
   }
 
-  setLanguage(language, idName) {
-    document.getElementById(idName).removeAttribute('filter', 'brightness(40%)');
-    var flagId = language === 'pl' ? 'english-flag' : 'polish-flag';
-    document.getElementById(flagId).setAttribute('filter', 'brightness(40%)')
-    document.documentElement.lang = language;
-    var langPath = document.documentElement.lang === 'pl' ? '/resumeData.json' : '/resumeDataEN.json';
-    this.getResumeData(langPath);
+  applyPickedLanguage(pickedLanguage, oppositeLangIconId) {
+    this.swapCurrentlyActiveLanguage(oppositeLangIconId);
+    document.documentElement.lang = pickedLanguage;
+    var resumePath =
+      document.documentElement.lang === window.$secondaryLanguage
+        ? "/res_primaryLanguage.json"
+        : "/res_secondaryLanguage.json";
+    this.loadResumeData(resumePath);
+  }
+
+  swapCurrentlyActiveLanguage(oppositeLangIconId) {
+    var pickedLangIconId =
+      oppositeLangIconId === window.$primaryLanguageIconId
+        ? window.$secondaryLanguageIconId
+        : window.$primaryLanguageIconId;
+    document
+      .getElementById(oppositeLangIconId)
+      .removeAttribute("filter", "brightness(40%)");
+    document
+      .getElementById(pickedLangIconId)
+      .setAttribute("filter", "brightness(40%)");
   }
 
   componentDidMount() {
-    this.setLanguage('en', 'english-flag');
+    this.applyPickedLanguage(
+      window.$primaryLanguage,
+      window.$secondaryLanguageIconId
+    );
   }
 
-  getResumeData(path) {
+  loadResumeData(path) {
     $.ajax({
       url: path,
-      dataType: 'json',
+      dataType: "json",
       cache: false,
       success: function (data) {
         this.setState({ resumeData: data });
@@ -49,29 +66,41 @@ class App extends Component {
       <div>
         <Header data={this.state.resumeData.main} />
         <div className="col-md-12 mx-auto text-center language">
-            <div
-              onClick={() => this.setLanguage('pl', 'polish-flag')}
-              style={{display:'inline'}}>
-              <span
-                className="iconify mr-5"
-                data-icon="twemoji-flag-for-flag-poland"
-                data-inline="false"
-                style={{ fontSize: '50px', cursor: 'pointer' }}
-                id="polish-flag"
-              ></span>
-            </div>
-            <div
-              onClick={() => this.setLanguage('en', 'english-flag')}
-              style={{display:'inline'}}>
-              <span
-                className="iconify"
-                data-icon="twemoji-flag-for-flag-united-kingdom"
-                data-inline="false"
-                style={{ fontSize: '50px', cursor: 'pointer' }}
-                id="english-flag"
-              ></span>
-            </div>
+          <div
+            onClick={() =>
+              this.applyPickedLanguage(
+                window.$primaryLanguage,
+                window.$secondaryLanguageIconId
+              )
+            }
+            style={{ display: "inline" }}
+          >
+            <span
+              className="iconify mr-5"
+              data-icon="twemoji-flag-for-flag-united-kingdom"
+              data-inline="false"
+              style={{ fontSize: "50px", cursor: "pointer" }}
+              id={window.$primaryLanguageIconId}
+            ></span>
           </div>
+          <div
+            onClick={() =>
+              this.applyPickedLanguage(
+                window.$secondaryLanguage,
+                window.$primaryLanguageIconId
+              )
+            }
+            style={{ display: "inline" }}
+          >
+            <span
+              className="iconify"
+              data-icon="twemoji-flag-for-flag-poland"
+              data-inline="false"
+              style={{ fontSize: "50px", cursor: "pointer" }}
+              id={window.$secondaryLanguageIconId}
+            ></span>
+          </div>
+        </div>
         <About data={this.state.resumeData.main} />
         <Projects data={this.state.resumeData.projects} />
         <Skills data={this.state.resumeData.resume} />
--- src/index.js
+++ src/index.js
@@ -5,5 +5,12 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+/* GLOBAL VARIABLES */
+
+window.$primaryLanguage = 'en';
+window.$secondaryLanguage = 'pl';
+window.$primaryLanguageIconId = 'primary-lang-icon';
+window.$secondaryLanguageIconId = 'secondary-lang-icon';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 serviceWorker.register();
