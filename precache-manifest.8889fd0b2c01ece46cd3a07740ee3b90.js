8f62434602de364587c8b616cc30b93407511a97
Dorota Gil <gildota@wp.pl>
2020-11-30 21:34:03 +0100

    Updates

---
+++ precache-manifest.8889fd0b2c01ece46cd3a07740ee3b90.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024dcddc87c2da6c51f60fad4e1635e4",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "152df70c56b4f22b17ba",
+    "url": "/react-frontend-dev-portfolio/static/css/2.a2524b97.chunk.css"
+  },
+  {
+    "revision": "e5831b3e96107373bcf2",
+    "url": "/react-frontend-dev-portfolio/static/css/main.a0676c96.chunk.css"
+  },
+  {
+    "revision": "152df70c56b4f22b17ba",
+    "url": "/react-frontend-dev-portfolio/static/js/2.38a1b32a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.38a1b32a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5831b3e96107373bcf2",
+    "url": "/react-frontend-dev-portfolio/static/js/main.754bfcb3.chunk.js"
+  },
+  {
+    "revision": "41327d6260852487720d",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.751851d5.js"
+  }
+]);
