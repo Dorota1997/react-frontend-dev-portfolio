9b1c28a9b799e0d34a3d10c95be0f7a691016494
pawel idzikowski <iddziku@gmail.com>
2021-06-05 21:10:13 +0200

    Updates

---
+++ precache-manifest.f5123b2bb91232354c32b563a661a77b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fce47fed5965eeae2bb12b22ce7eb5f1",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "71ccc6b40de2bc30f7ee",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d5cacb0c235df2bfaa8e",
+    "url": "/react-frontend-dev-portfolio/static/css/main.fcf2461e.chunk.css"
+  },
+  {
+    "revision": "71ccc6b40de2bc30f7ee",
+    "url": "/react-frontend-dev-portfolio/static/js/2.67049b73.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.67049b73.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5cacb0c235df2bfaa8e",
+    "url": "/react-frontend-dev-portfolio/static/js/main.9ff1d112.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.8e694c3511f7300ffe182ea5460a7661.js"
+  "/react-frontend-dev-portfolio/precache-manifest.f5123b2bb91232354c32b563a661a77b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
