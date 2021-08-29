2fc2c206ba8ba2a331d22c592a61d9c083d23cfe
Christian Plitz <christian.plitz@tum.de>
2021-08-29 17:03:09 +0200

    Updates

---
+++ precache-manifest.0b43e95031d4622e0de738b47f7853b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d6258b2e48529b90d7a85054510fb0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe70c0d5621f5cf0ed27",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "d15ac9b67162a4cf5572",
+    "url": "/static/css/main.fcf2461e.chunk.css"
+  },
+  {
+    "revision": "fe70c0d5621f5cf0ed27",
+    "url": "/static/js/2.1ca1a3d8.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.1ca1a3d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d15ac9b67162a4cf5572",
+    "url": "/static/js/main.af050ff7.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
