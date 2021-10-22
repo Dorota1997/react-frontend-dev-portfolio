7fdefa713db58046cee1352903da413f26130fab
Trí Phạm <minhtri2805@gmail.com>
2021-10-22 11:31:33 -0500

    Updates

---
+++ precache-manifest.a383627fc77ad553a250dfa9bb641a31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9285a012bdf342098175e7d36f941bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75c1377a0f7754613b99",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "5e2021f8cb6f5bcf0814",
+    "url": "/static/css/main.4fed58f8.chunk.css"
+  },
+  {
+    "revision": "75c1377a0f7754613b99",
+    "url": "/static/js/2.858ee36a.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.858ee36a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e2021f8cb6f5bcf0814",
+    "url": "/static/js/main.1d0b6a20.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
