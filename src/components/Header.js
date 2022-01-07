fa8a67b21991a4c8cde6286759fc3249325a4807
ily123 <ibnovikov@gmail.com>
2022-01-07 17:24:05 -0600

    Removed the 140px crop from header.
    This accounts for the removed language-select div that was 140px tall.

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,7 +35,7 @@ class Header extends Component {
     }, (props, prevProp) => true);
 
     return (
-      <header id="home" style={{ height: window.innerHeight - 140, display: 'block' }}>
+      <header id="home" style={{ height: window.innerHeight, display: 'block' }}>
         <div className="row aligner" style={{height: '100%'}}>
           <div className="col-md-12">
             <div>
