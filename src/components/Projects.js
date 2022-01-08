b06d26715ebf5d59274468eb7607a24aa41ae799
ily123 <ibnovikov@gmail.com>
2022-01-07 18:40:43 -0600

    Changed project card size from 4 to 3.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -21,7 +21,7 @@ class Projects extends Component {
       var projects = this.props.resumeProjects.map(function (projects) {
         return (
           <div
-            className="col-sm-12 col-md-6 col-lg-4"
+            className="col-sm-12 col-md-6 col-lg-3"
             key={projects.title}
             style={{ cursor: "pointer" }}
           >
