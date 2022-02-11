8b74575d3eb99a497f4282ffcd63f48bdca46441
diaz-camilo <diaz.camilo@icloud.com>
2022-02-11 11:14:07 +1100

    refactored components to functional components using hooks

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,70 +1,69 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import ProjectDetailsModal from "./ProjectDetailsModal";
 
-class Projects extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      deps: {},
-      detailsModalShow: false,
-    };
-  }
+function Projects(props) {
+  const [deps, setDeps] = useState({});
+  const [detailsModalShow, setDetailsModalShow] = useState(false);
 
-  render() {
-    let detailsModalShow = (data) => {
-      this.setState({ detailsModalShow: true, deps: data });
-    };
+  const handleDetailsModalShowClick = (data) => {
+    setDeps(data);
+    setDetailsModalShow(true);
+  };
 
-    let detailsModalClose = () => this.setState({ detailsModalShow: false });
-    if (this.props.resumeProjects && this.props.resumeBasicInfo) {
-      var sectionName = this.props.resumeBasicInfo.section_name.projects;
-      var projects = this.props.resumeProjects.map(function (projects) {
-        return (
-          <div
-            className="col-sm-12 col-md-6 col-lg-4"
-            key={projects.title}
-            style={{ cursor: "pointer" }}
-          >
-            <span className="portfolio-item d-block">
-              <div className="foto" onClick={() => detailsModalShow(projects)}>
-                <div>
-                  <img
-                    src={projects.images[0]}
-                    alt="projectImages"
-                    height="230"
-                    style={{marginBottom: 0, paddingBottom: 0, position: 'relative'}}
-                  />
-                  <span className="project-date">{projects.startDate}</span>
-                  <br />
-                  <p className="project-title-settings mt-3">
-                    {projects.title}
-                  </p>
-                </div>
+  const detailsModalClose = () => setDetailsModalShow(false);
+  if (props.resumeProjects && props.resumeBasicInfo) {
+    var sectionName = props.resumeBasicInfo.section_name.projects;
+    var projects = props.resumeProjects.map(function (projects) {
+      return (
+        <div
+          className="col-sm-12 col-md-6 col-lg-4"
+          key={projects.title}
+          style={{ cursor: "pointer" }}
+        >
+          <span className="portfolio-item d-block">
+            <div
+              className="foto"
+              onClick={() => handleDetailsModalShowClick(projects)}
+            >
+              <div>
+                <img
+                  src={projects.images[0]}
+                  alt="projectImages"
+                  height="230"
+                  style={{
+                    marginBottom: 0,
+                    paddingBottom: 0,
+                    position: "relative",
+                  }}
+                />
+                <span className="project-date">{projects.startDate}</span>
+                <br />
+                <p className="project-title-settings mt-3">{projects.title}</p>
               </div>
-            </span>
-          </div>
-        );
-      });
-    }
-
-    return (
-      <section id="portfolio">
-        <div className="col-md-12">
-          <h1 className="section-title" style={{ color: "black" }}>
-            <span>{sectionName}</span>
-          </h1>
-          <div className="col-md-12 mx-auto">
-            <div className="row mx-auto">{projects}</div>
-          </div>
-          <ProjectDetailsModal
-            show={this.state.detailsModalShow}
-            onHide={detailsModalClose}
-            data={this.state.deps}
-          />
+            </div>
+          </span>
         </div>
-      </section>
-    );
+      );
+    });
   }
+
+  return (
+    <section id="portfolio">
+      <div className="col-md-12">
+        <h1 className="section-title" style={{ color: "black" }}>
+          <span>{sectionName}</span>
+        </h1>
+        <div className="col-md-12 mx-auto">
+          <div className="row mx-auto">{projects}</div>
+        </div>
+        <ProjectDetailsModal
+          show={detailsModalShow}
+          onHide={detailsModalClose}
+          data={deps}
+        />
+      </div>
+    </section>
+  );
 }
 
 export default Projects;
