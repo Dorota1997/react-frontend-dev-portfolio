12a6ee15cc39982a989cde1137138bf591630f16
Soheil Qorbani <soheil.q.k@gmail.com>
2022-02-20 01:57:38 +0330

    Updates

---
+++ precache-manifest.4a6fe05b72151f0792398b93a8495e47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bffee6150d29ddfce87703492ea83e5a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "952887eae291767abcd3",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "1c5f7f855cdd2d7b73eb",
+    "url": "/static/css/main.eed97966.chunk.css"
+  },
+  {
+    "revision": "952887eae291767abcd3",
+    "url": "/static/js/2.b7a97dc8.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.b7a97dc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c5f7f855cdd2d7b73eb",
+    "url": "/static/js/main.a14e5c28.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.617f0b8a52a62e7bfd104d6d39b9ce6e.js"
+  "/precache-manifest.4a6fe05b72151f0792398b93a8495e47.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
