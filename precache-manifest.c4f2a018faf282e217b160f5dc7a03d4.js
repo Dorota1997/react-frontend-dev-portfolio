43d3729abc85905a86736debb9779f37b6d59318
WIN\gmral <gmralwnr@hanmail.net>
2022-05-18 23:14:05 +0900

    Updates

---
+++ precache-manifest.c4f2a018faf282e217b160f5dc7a03d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1acb8f1d39ceacb2d7fed5c81fffe88d",
+    "url": "/minji.gihub.io/index.html"
+  },
+  {
+    "revision": "0d0d3c22f91aba1d3e2a",
+    "url": "/minji.gihub.io/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "f3a3f67e95581a3b569e",
+    "url": "/minji.gihub.io/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "0d0d3c22f91aba1d3e2a",
+    "url": "/minji.gihub.io/static/js/2.2ea329a9.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/minji.gihub.io/static/js/2.2ea329a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3a3f67e95581a3b569e",
+    "url": "/minji.gihub.io/static/js/main.130a4c18.chunk.js"
+  },
+  {
+    "revision": "eaa38b9fd3820301d1c8",
+    "url": "/minji.gihub.io/static/js/runtime-main.efc00971.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.ee50bc0a1fbb4aafab17a8b6801d6718.js"
+  "/minji.gihub.io/precache-manifest.c4f2a018faf282e217b160f5dc7a03d4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/minji.gihub.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
