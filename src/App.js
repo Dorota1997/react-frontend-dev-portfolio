5df06a7f1f06613515427ba9b0f8e58ade569c5e
Ruchika Gosain <sharma.ruchika34@gmail.com>
2022-07-18 22:36:29 -0400

    update the app.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
   constructor(props) {
     super();
     this.state = {
-      foo: "bar",
+      foo: "barss",
       resumeData: {},
       sharedData: {},
     };
