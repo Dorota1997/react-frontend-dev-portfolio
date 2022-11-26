d3131da9f78f8b32bf40b410b8de877f3c7f2efc
Christian Plitz <christian.plitz@tum.de>
2022-11-26 14:53:14 -0300

    Updates

---
+++ precache-manifest.c3bd461363d379d1234e085670b97078.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d1c30491022eabdcaa91fcf22092cf",
+    "url": "/react-frontend-dev-portfolio/index.html"
+  },
+  {
+    "revision": "4616bc8b2d159f244653",
+    "url": "/react-frontend-dev-portfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "58de869da2fe24e2c172",
+    "url": "/react-frontend-dev-portfolio/static/css/main.e5173af5.chunk.css"
+  },
+  {
+    "revision": "4616bc8b2d159f244653",
+    "url": "/react-frontend-dev-portfolio/static/js/2.3f3308d5.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/react-frontend-dev-portfolio/static/js/2.3f3308d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58de869da2fe24e2c172",
+    "url": "/react-frontend-dev-portfolio/static/js/main.b57947c2.chunk.js"
+  },
+  {
+    "revision": "8b91e79b160a90e30d4f",
+    "url": "/react-frontend-dev-portfolio/static/js/runtime-main.0af024f6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f8256441be67a8a7d1804eb472651421.js"
+  "/react-frontend-dev-portfolio/precache-manifest.c3bd461363d379d1234e085670b97078.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
