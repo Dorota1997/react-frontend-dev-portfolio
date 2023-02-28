7134f656f994b71a934921de4f9f06ca2bea36b7
Victoria <vic_figueroa08@yahoo.com>
2023-02-28 17:28:20 -0500

    updated experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -33,7 +33,7 @@ class Experience extends Component {
             className="vertical-timeline-element--work"
             date={work.years}
             iconStyle={{
-              background: "#AE944F",
+              background: "#F0DBDB",
               color: "#fff",
               textAlign: "center",
             }}
@@ -78,7 +78,7 @@ class Experience extends Component {
             {work}
             <VerticalTimelineElement
               iconStyle={{
-                background: "#AE944F",
+                background: "#F0DBDB",
                 color: "#fff",
                 textAlign: "center",
               }}
