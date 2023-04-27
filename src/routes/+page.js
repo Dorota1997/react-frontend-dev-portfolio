1877bc7160e3c9a056f89ca5f4903b844da3714a
Sean Redmon <seanredmon@seanred.io>
2023-04-27 11:36:10 -0400

    fixed waterfall problem

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,21 +1,21 @@
 import { client, urlFor } from "$lib/client";
-export const prerender = true;
 
-export async function load({ params }) {
-  const pageInfo = await client.fetch(`*[_type == "info"][0]`);
-  pageInfo.bioImage = urlFor(pageInfo.bioImage).width(250).url();
-  const projects = await client.fetch(
-    `*[_type == "project"] | order(order asc)`
-  );
+export async function load() {
+  const fetchPageInfo = async () => {
+    const pageInfo = await client.fetch(`*[_type == "info"][0]`);
+    pageInfo.bioImage = urlFor(pageInfo.bioImage).width(250).url();
+    return pageInfo;
+  };
 
-  if (pageInfo && projects) {
-    return {
-      pageInfo,
-      projects,
-    };
-  }
+  const fetchProjects = async () => {
+    const projects = await client.fetch(
+      `*[_type == "project"] | order(order asc)`
+    );
+    return projects;
+  };
+
   return {
-    status: 500,
-    body: new Error("Internal Server Error"),
+    pageInfo: fetchPageInfo(),
+    projects: fetchProjects(),
   };
 }
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,16 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
+import { visualizer } from "rollup-plugin-visualizer";
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	optimizeDeps: {
-		include: ['lodash.get', 'lodash.isequal', 'lodash.clonedeep']
-	}
+  plugins: [
+    sveltekit(),
+    visualizer({
+      emitFile: true,
+      filename: "stats.html",
+    }),
+  ],
+  optimizeDeps: {
+    include: ["lodash.get", "lodash.isequal", "lodash.clonedeep"],
+  },
 });
